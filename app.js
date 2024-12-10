be503a17db88829e77f4b8f5f7308a3f6359085b
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-10 14:57:08 +0100

    create all functions (Additions, Soustraction, Multipilation, Racinne-carre, sqrt, power)

---
+++ app.js
@@ -0,0 +1,43 @@
+
+function Addition(a, b) {
+    return a + b;
+}
+
+function Soustraction(a, b) {
+    return a - b;
+}
+
+function Multiplication(a, b) {
+    return a * b;
+}
+
+function Division(a, b) {
+    if (b != 0) {
+        console.log("Division par zero!!!");
+    } else return a / b;
+}
+
+function Puissance(a, b) {
+    return a ** b;
+}
+
+function Racine_carree(a) {
+    return Math.sqrt(a);
+}
+
+function Factorielle(n) {
+    if (n < 0) {
+      return 'La factorielle n\'est pas \
+        définie pour les nombres négatifs.';
+    }
+    let resultat = 1;
+    for (let i = 1; i <= n; i++) {
+      resultat *= i;
+    }
+    return resultat;
+}
+  
+
+
+
+
