6001353979f32a313b06013b76bd71323d080a9d
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-11 11:21:05 +0100

    make switch for the user

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const ps = require("prompt-sync"); // // import the prompt-sync module via npm
+const prompt = ps(); // create prompt function
+
+const reset = '\x1b[0m';
+const red = '\x1b[31m';
+const green = '\x1b[32m';
+const yellow = '\x1b[33m';
+
+
 
 function Addition(a, b) {
     return a + b;
@@ -37,16 +46,72 @@ function Factorielle(n) {
     return resultat;
 }
 
+function Menu() {
+    console.log("1. Addition");
+    console.log("2. Soustraction");
+    console.log("3. Multiplication");
+    console.log("4. Division");
+    console.log("5. Puissance");
+    console.log("6. Racine_carree");
+    console.log("7. Factorielle");
+    console.log("8. Quitter");
+}
 
-// Import readlines modulo
-const readlineInterface = require("readline");
-// create a communication between terminal use createInterface
-const r1 = readlineInterface.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-r1.question("Enterz Votre Choix: ", function(choix) {
-    console.log(choix);
-});
+
+
+function Calculator() {
+    Menu();
+    let choix = prompt("Enterz Votre Choix: ");
+    choix = Number(choix);
+    switch(choix) {
+        case 1: {
+            let n1 = prompt("Enterz Premier nomber: ");
+            let n2 = prompt("Enterz Deuxieme nomber: ");
+            console.log(green, "Resultat = ", String(Addition(Number(n1), Number(n2))), reset);
+            break;
+        }
+        case 2: {
+            let n1 = prompt("Enterz Premier nomber: ");
+            let n2 = prompt("Enterz Deuxieme nomber: ");
+            console.log("Resultat = ", Soustraction(Number(n1), Number(n2)));
+            break;
+        }
+        case 3: {
+            let n1 = prompt("Enterz Premier nomber: ");
+            let n2 = prompt("Enterz Deuxieme nomber: ");
+            console.log("Resultat = ", Multiplication(Number(n1), Number(n2)));
+            break;
+        }
+        case 4: {
+            let n1 = prompt("Enterz Premier nomber: ");
+            let n2 = prompt("Enterz Deuxieme nomber: ");
+            console.log("Resultat = ", Division(Number(n1), Number(n2)));
+            break;
+        }
+        case 5: {
+            let n1 = prompt("Enterz Premier nomber: ");
+            let n2 = prompt("Enterz Deuxieme nomber: ");
+            console.log("Resultat = ", Puissance(Number(n1), Number(n2)));
+            break;
+        }
+        case 6: {
+            let n1 = prompt("Enterz un nomber: ");
+            console.log("Resunltat = ", Racine_carree(Number(n1)));
+            break;
+        }
+        case 7: {
+            let n1 = prompt("Enterz u nomber: ");
+            console.log("Resultat = ", Factorielle(Number(n1)));
+            break;
+        }
+        case 8: {
+            return; // break the function execution
+        }
+        default: console.log("Le Nombre Invalide!!!");
+    }
+    Calculator();
+    
+}
+Calculator();
 
 
--- src.js
+++ src.js
@@ -1,19 +1,26 @@
-// NodeJs & npm
+// NodeJs & npm & require
 /*
+    require:
+        always return a function
+        It loads and uses code from other files or packages.
+
     Node: 
         allow you tu run javascript outside
         the web browser (in the terminal)
 
     npm:
-        NPM (Node Package Manager) is a tool that comes
-        with Node.js, used to install, manage, 
+        NPM (Node Package Manager), used to install, manage, 
         and share JavaScript libraries and packages.
-*/
+        like github: contain a lot of package
+        npm: contain a lot of packages like "prompt-sync"
 
-// User Input
-/*
-    Option 1: readline
-    Option 2: prompt-sync --> install with npm
+        Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+            change the PowerShell policy to run any script
+        PowerShell: blocks some scripts to protect the computer
+
+    package a lot of modulo and modulo contain a lot of libraries
+
+    "in js you can store function in variable & use variable as function"
 */
 
 
